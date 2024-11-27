c975f9ba187d4d00821bd1ecb8d6154c2814e107
valentin prost <valentinprost56@gmail.com>
2024-11-27 11:16:23 +0100

    Add Setters/toString + test

--- Index.php
+++ Index.php
@@ -6,14 +6,21 @@ spl_autoload_register(function($classe)
 $p1 = new PieceEchec(5,5,1);
 $p1->getCouleur();
 $p1->getCouleurCase();
+echo $p1;
+echo '<br>';
 $p2 = new PieceEchec(4,5,2);
 $p2->getCouleur();
 $p2->getCouleurCase();
 
+echo $p2;
+echo '<br>';
 // Pieces invalides (coordonées incorrectes)
-$p3 = new PieceEchec(9,5,1);
+$p3 = new PieceEchec(1,3,1);
 $p3->getCouleur();
 $p3->getCouleurCase();
+echo $p3;
+echo '<br>';
+
 
 // Pieces invalides (Couleur)
 $p4 = new PieceEchec(1,5,50);
--- classes/PieceEchec.class.php
+++ classes/PieceEchec.class.php
@@ -18,15 +18,48 @@ class PieceEchec {
      {  if ($couleur == 1 || $couleur == 2) {
         $this->couleur = $couleur; 
     } else {
-        throw new Exception('Couleur de la piece inconnue <br>');
+        throw new Exception('Couleur de la piece inconnue');
     }
-        if (($x < 1 || $x > 8)||($y < 1 || $y > 8)) {
-            throw new Exception('Coordonées de la piece incorrectes<br>');
-        } else {
-            $this ->x = $x;
-            $this ->y = $y;
-        }
+        // if (($x < 1 || $x > 8)||($y < 1 || $y > 8)) {
+        //     throw new Exception('Coordonées de la piece incorrectes');
+        // } else {
+            $this ->x = $this->setX($x);
+            $this ->y = $this->setY($y);
+        // }
      }
+     // --------------------LES SETTERS --------------
+
+     /** Fonction setX 
+      * Modifie la valeur x
+      *
+      * @return int
+      */
+     
+        public function setX($x) : int {
+            if ($x < 1 || $x > 8) {
+                throw new Exception('Coordonées "X" de la piece incorrectes');
+            } else {
+                $this ->x = $x;
+                return $x;
+            }
+        }
+
+        /** Fonction setY
+      * Modifie la valeur y
+      * 
+      * @return int
+      */
+     
+      public function setY($y) : int {
+        if ($y < 1 || $y > 8) {
+            throw new Exception('Coordonées "Y" de la piece incorrectes');
+        } else {
+            $this ->y = $y;
+            return $y;
+        }
+    }
+
+     // --------------------LES GETTERS --------------
 
      //Fonction getCouleur
     /**
@@ -53,13 +86,30 @@ class PieceEchec {
 
       public function getCouleurCase() :int {
         if (($this->x + $this->y) % 2 == 0 ){
-            echo 'La case est Noire <br><br> ';
+            echo 'La case est Noire <br> ';
             return 2;
         } else {
-            echo 'La case est blanche<br><br>';
+            echo 'La case est blanche<br>';
             return 1;
         }
       }
+
+      //function toString
+      /**
+       * 
+       */
+
+       public function __toString() : string
+       {
+        $msg = 'Coordonées : x'.$this->x;
+        $msg .=' ; y'.$this->y;
+        if ($this->couleur == 1) {
+            $msg .= '. Couleur : Blanc';
+        } else {
+            $msg .= '. Couleur : Noir';
+        }
+        return $msg .='<br>';
+       }
 
 
 
